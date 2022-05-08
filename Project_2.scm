a0a6e27583e4cf17646623c77dfcc9a8f7dec4ff
Deepankar Chakraborty <52084764+deepankarck2@users.noreply.github.com>
2022-05-07 21:35:52 -0400

    Added selectors

--- Project_2.scm
+++ Project_2.scm
@@ -1,9 +1,11 @@
-(define a (list '-x 'v 'y))
+;(define a (list '-x 'v 'y))
 
 (define (make-and f s)
   (cons f (cons '^ (cons s '() ))))
+
 (define (make-or f s)
   (cons f (cons 'v (cons s '() ))))
+
 (define (make-not f s)
   (cons f (cons '- (cons s '() ))))
 
@@ -14,20 +16,26 @@
   (car clauses))
 
 (define (second-operand clauses)
-  (
-   
-   p1 v p2
+  (caddr clauses))
+
+(define (classifier clauses)
+  (cadr clauses))
+
+(display "Display Infos\n")
+
+(first-operand (make-and 'p1 'p2))    ;p1
+(second-operand (make-and 'p1 'p2))   ;p2
+(classifier (make-and 'p1 'p2))       ;^
 
 
 
 
 
 
+;(define (make-and-list lst)
+ ; (display lst)
+  ; )
 
-(define (make-and-list lst)
-  (display lst)
-   )
-
-(make-not 3 6)
-(make-not (list '- 'p1) 'p2)
+;(make-not 3 6)
+;(make-not (list '- 'p1) 'p2)
 ;(make-and-list '(-2 v -2))
