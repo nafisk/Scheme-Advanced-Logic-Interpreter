83f247d71a6621a2afadbeebb2ec811e8b7d67ff
Deepankar Chakraborty <52084764+deepankarck2@users.noreply.github.com>
2022-05-15 11:07:05 -0400

    added others

--- Project_2.scm
+++ Project_2.scm
@@ -1,4 +1,11 @@
+
 ;(define a (list '-x 'v 'y))
+
+;; p= (x v y)
+;; Q = (not x ^ y)
+;; p => Q
+
+
 
 (define (make-and f s)
   (cons f (cons '^ (cons s '() ))))
@@ -6,10 +13,36 @@
 (define (make-or f s)
   (cons f (cons 'v (cons s '() ))))
 
-(define (make-not f s)
-  (cons f (cons '- (cons s '() ))))
+(define (make-not f)
+  (cons '- (cons f '())))
+; TODO - if ! already present, cancel it out
+
+(define (make-imply f s)
+  (cons f (cons '=> (cons s '() ))))
 
 
+(define P (make-or 'x 'y))
+(define Q (make-and (make-not 'x) 'y))
+
+;(make-or 'x 'y)
+
+(define inpt (make-imply P Q))
+
+;;  [x → y] ⇛ (!x v y) 
+
+(define (simp input)
+  (cond ((eq? '=> (classifier input))
+         ((make-or (make-not (first-operand input)) (second-operand input))))
+        
+        (else #f)))
+
+
+
+
+
+
+
+(display "\n")
 
 
 (define (first-operand clauses)
@@ -26,6 +59,9 @@
 (first-operand (make-and 'p1 'p2))    ;p1
 (second-operand (make-and 'p1 'p2))   ;p2
 (classifier (make-and 'p1 'p2))       ;^
+
+(make-not 'p1)
+
 
 
 
